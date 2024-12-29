f50977c1edee3aadec8a629244dc39b9f1fa7f6e
Mark-Court <courtademark#gmail.com>
2024-12-29 15:59:55 -0700

    Main layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-
+import { Route, Routes } from "react-router-dom";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import HomePage from "./pages/home/HomePage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 export default function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback
+        signUpForceRedirectUrl={"/auth-callback"}
+        />} />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      </Routes>
+    </>
   );
 }
