b30a8c7837d470d8971294f1543940fe2be8b72b
Mark-Court <courtademark#gmail.com>
2024-12-29 18:08:57 -0700

    finished left side bar

---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,43 @@
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import { Outlet } from "react-router-dom";
+import LeftSideBar from "./components/LeftSideBar";
+
+const MainLayout = () => {
+  const isMoble = false;
+
+  return (
+    <div className="h-screen bg-black text-white flex flex-col">
+      <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden p-2">
+        {/* Left sidebar */}
+        <ResizablePanel
+          defaultSize={20}
+          minSize={isMoble ? 0 : 10}
+          maxSize={30}
+        >
+          <LeftSideBar />
+        </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+        {/* Main content */}
+        <ResizablePanel defaultSize={isMoble ? 80 : 60}>
+          <Outlet />
+        </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+        {/* Right sidebar */}
+        <ResizablePanel
+          defaultSize={20}
+          minSize={0}
+          maxSize={20}
+          collapsedSize={0}
+        >
+          Friends activity
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+};
+
+export default MainLayout;
